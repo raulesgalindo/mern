916d5e0681bae9fe659ac2f9ec5131e4042de9ca
Raul Galindo <raulgalindorosales@gmail.com>
2020-08-25 09:04:56 -0600

    Added redux, uuid and started to work with actions, reducers

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import React, {Fragment} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
 import Landing from './components/Layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import './App.css';
 
-const App = () => 
-  <Router>
-  <Fragment>
-    <Navbar/>
-    <Route exact path='/' component={Landing}/>
-    <section className='container'>
-      <Switch>
-        <Route exact path='/register' component={Register}/>
-        <Route exact path='/login' component={Login}/>
-      </Switch>
-    </section>
-  </Fragment>
-  </Router>
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+);
 
 export default App;
