a7c37786c063ca1b2d33076bd2b414262ed54cb9
Raul Galindo <raulgalindorosales@gmail.com>
2020-08-29 12:45:49 -0600

    added action and reducers for the alerts, added an alert for the register user

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Navbar from './components/Layout/Navbar';
 import Landing from './components/Layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/Layout/Alert';
+
 import './App.css';
 
 //Redux
@@ -17,6 +19,7 @@ const App = () => (
         <Navbar />
         <Route exact path='/' component={Landing} />
         <section className='container'>
+          <Alert />
           <Switch>
             <Route exact path='/register' component={Register} />
             <Route exact path='/login' component={Login} />
